23c87343c0ee13ffa19415e3c5ec2652b517fb1f
hermansengaute <78079472+hermansengaute@users.noreply.github.com>
2021-03-17 12:32:59 +0100

    Update mapscript.js

--- src/mapscript.js
+++ src/mapscript.js
@@ -1,32 +1,50 @@
 
+
+var[] xPos = {59.22022506832878, 59.199803985842124, 59.21368429502667, 51.4};
+var[] yPos = {9.615463280937659, 9.595800722793395, 9.585710132976175, -0.09};
+
+
+
 var liste = {
-	0:{hus:"Rødt Hus",x:51.1, y:-0.09, beskriv:"<br> Den er bra husen ja"}, 
-	1:{hus:"Fiolett hus",x:51.2, y:-0.09, beskriv:"<br> Den nesten bra husen ja"}, 
-	2:{hus:"Blått hus",x:51.3, y:-0.09, beskriv:"<br> Den er bra husen ja"}, 
+	0:{hus:"Rødt Hus",x:59.22022506832878, y:9.615463280937659, beskriv:"<br> Den er bra husen ja"}, 
+	1:{hus:"Fiolett hus",x:59.199803985842124, y:9.595800722793395, beskriv:"<br> Den nesten bra husen ja"}, 
+	2:{hus:"Blått hus",x:59.21368429502667, y:9.585710132976175, beskriv:"<br> Den er bra husen ja"}, 
 	3:{hus:"Gult hus",x:51.4, y:-0.09, beskriv:"<br> Den er bra husen ja"}
 };
 
-var greenIcon = L.icon({
-    iconUrl: 'leaf-green.png',
+var interest = {
+	0:{hus:"Dominos Pizza",x:59.2094436, y:9.602231, beskriv:"<br> VeldeBRA PIZZE"}, 
+	1:{hus:"Hanken Barnehage",x:59.2085648, y:9.6018662, beskriv:"<br> Masse lekre barn ;)"}, 
+	2:{hus:"Blått hus",x:59.2101218, y:9.6207422, beskriv:"<br> Den er bra husen ja"}, 
+	3:{hus:"Gult hus",x:59.2020116, y:9.6153242, beskriv:"<br> Den er bra husen ja"}
+};
 
-    iconSize:     [38, 95], // size of the icon
-    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+var redIcon = L.icon({
+	iconUrl: 'redMark.png',
+	iconSize: [40, 40], // ikon-størrelse
+	iconAnchor: [40/2, 40] // ikon-anker. Der iconet treffer kartet
 });
 
-var map = L.map('mapid').setView([59.412369, 9.067760], 15); 
-//59°17′26″N 11°12′10″Ø
-
-L.marker([59.5, 9.09], {icon: greenIcon}).addTo(map);
+var map = L.map('mapid').setView([59.2089493, 9.6022095], 15); 
+//SKIEN
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map); 
 
 
+//for salgsobjekter
 Object.keys(liste).forEach((v)=>{
-L.marker([liste[v].x, liste[v].y]).addTo(map)
+L.marker([liste[v].x, liste[v].y], {icon: redIcon}).addTo(map)
     .bindPopup(liste[v].hus + liste[v].beskriv);
+    // .openPopup();
+});
+
+
+//For interessepunkter
+Object.keys(interest).forEach((v)=>{
+L.marker([interest[v].x, interest[v].y]).addTo(map)
+    .bindPopup(interest[v].hus + interest[v].beskriv);
     // .openPopup();
 });
 
