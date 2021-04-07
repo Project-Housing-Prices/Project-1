7d2edf9d7afbfc5ad350b5084f39686a02214fab
Linrik <77327567+Linrik@users.noreply.github.com>
2021-04-07 11:39:07 +0200

    Add files via upload

---
+++ src/bolig.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>kart</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="Nettsted mal">
+	<meta name="keywords" content="mal, nettsted">
+	<meta name="author" content="Henrik">
+	<link rel="stylesheet" href="leaflet.css" />
+	<script src="leaflet.js"></script>
+	<script type="text/javascript" src="jq.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="mapid"></div>
+	<p class="fornavn"></p>
+</body>
+</html>
